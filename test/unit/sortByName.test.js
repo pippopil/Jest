dab18a8af9ab64a735da249c8f9ea9b5149ba4f4
pippopil <voood234@gmail.com>
2022-09-19 16:14:20 +0300

    Add

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,24 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+it("Books with the same title remain unsorted", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Властелин Колец", "Гарри Поттер"])
+    ).toEqual(["Властелин Колец", "Гарри Поттер", "Гарри Поттер"]);
+  });
+
+});
